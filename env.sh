4d6bb9e0a1371f3edf12856bd495edc6624112c5
Keval <4792353+kevalshah90@users.noreply.github.com>
2018-08-26 23:18:15 -0700

    Configure env.sh

--- env.sh
+++ env.sh
@@ -5,12 +5,12 @@ ssh-keygen -t rsa && ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub && eval 
 
 ###################### GIT PARAMETERS #####################################
 export GIT_PARENT_DIR=~
-export GIT_REPO_NAME=#your-repo
-export GIT_BRANCH_NAME=#your-branch
-export GIT_USER=#your-gituser
-export GIT_EMAIL=#your-email
-export GITHUB_ACCESS_TOKEN=#access-token from github developer settings
-export GIT_USER_UPSTREAM=#your-user
+export GIT_REPO_NAME=StroomProduct
+export GIT_BRANCH_NAME=Master
+export GIT_USER=kevalshah90
+export GIT_EMAIL=kevalshah90@gmail.com
+export GITHUB_ACCESS_TOKEN=0f68a548c883adcb196f55efad137665a36acf3c
+export GIT_USER_UPSTREAM=kevalshah90
 
 
 ############################################################################
